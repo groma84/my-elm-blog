208d73d887327bfaf58f757b51a2962f2af19a9a
Martin Grotz <martin.grotz@gmx.de>
2019-02-03 15:50:59 +0100

    Links zu github&Co. und eine Events&Artikel Seite ergänzt

--- menu-component.js
+++ menu-component.js
@@ -13,41 +13,72 @@ function getActivePage(pageName) {
 
 const MenuComponent = {
     render: () => html`
-    <nav>
-    <ul>
-        <li class="${getActivePage('index') && 'active'}">
-            <a href="index.html">Blog</a>
-        </li>
-        <li class="${getActivePage('faq') && 'active'}">
-            <a href="faq.html">FAQ</a>
-        </li>
-        <li class="${getActivePage('datenschutz') && 'active'}">
-            <a href="datenschutz.html">Datenschutz</a>
-        </li>
-        <li class="${getActivePage('impressum') && 'active'}">
-            <a href="impressum.html">Impressum</a>
-        </li>
-    </ul>
-</nav>
+    <div class="menu">
+        <nav>
+            <ul>
+                <li class="${getActivePage('index') && 'active'}">
+                    <a href="index.html">Blog</a>
+                </li>
+                <li class="${getActivePage('events') && 'active'}">
+                    <a href="events.html">Events & Termine</a>
+                </li>
+                <li class="${getActivePage('faq') && 'active'}">
+                    <a href="faq.html">FAQ</a>
+                </li>
+                <li class="${getActivePage('datenschutz') && 'active'}">
+                    <a href="datenschutz.html">Datenschutz</a>
+                </li>
+                <li class="${getActivePage('impressum') && 'active'}">
+                    <a href="impressum.html">Impressum</a>
+                </li>
+            </ul>
+        </nav>
 
-<style>
-nav > ul {
-    display: flex;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-  
-  nav > ul > li {
-    margin: 0.5rem;
-    padding: 0.5rem;
-    background-color: lightblue;
-  }
+        <aside class="social_media_stuff">
+            <ul>
+                <li><a href="https://github.com/groma84">Github</a></li>
+                <li><a href="https://twitter.com/mobilgroma">Twitter</a></li>
+                <li><a href="https://www.xing.com/profile/Martin_Grotz3/cv">Xing</a></li>
+            </ul>
+        </aside>
+    </div>
 
-  nav > ul > li.active {
-      font-weight: bold;
-  }
-  
+    <style>
+        .menu {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .social_media_stuff > ul {
+            display: flex;
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        .social_media_stuff > ul > li {
+            margin: 0.5rem;
+            padding: 0.5rem;
+            background-color: lightblue;
+        }
+
+        nav > ul {
+            display: flex;
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+        
+        nav > ul > li {
+            margin: 0.5rem;
+            padding: 0.5rem;
+            background-color: lightblue;
+        }
+
+        nav > ul > li.active {
+            font-weight: bold;
+        }
+    
     </style>
     `
 };
